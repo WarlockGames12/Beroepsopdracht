1b509b8d5bd581d1ffd523e8cb2effabe7ffdfa8
WarlockGames12 <47526227+WarlockGames12@users.noreply.github.com>
2020-09-16 11:34:26 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,18 @@
+print("Hello you, Ik ben Dirk ten Have")
+print("Kunt u mij vertellen wie u bent?")
+
+username = input("")
+print("Mijn naam is " + username)
+
+import datetime
+x = datetime.datetime.now()
+
+print(x)
+
+answer = input("Enter yes or no: ") 
+if answer == "yes": 
+    print("Pizza Time") 
+elif answer == "no": 
+    print("Stares Motherfuckerly") 
+else: 
+    print("Please enter yes or no.")
